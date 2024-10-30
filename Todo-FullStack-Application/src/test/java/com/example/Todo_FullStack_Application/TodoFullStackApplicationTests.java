a03f6955939e2dda8d5e6eb4adc1d0e7f0fbef5d
lahiruprabath701 <135402608+lahiruprabath701@users.noreply.github.com>
2024-10-30 22:28:12 +0530

    Initial commit

---
+++ Todo-FullStack-Application/src/test/java/com/example/Todo_FullStack_Application/TodoFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Todo_FullStack_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
